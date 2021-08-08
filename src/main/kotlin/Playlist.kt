5073234b2c3dedad4b08598225d983641781b0ac
Denis Kiura <kiura@hokela.co.ke>
2021-08-08 10:22:22 +0300

    implementing the interface

--- src/main/kotlin/Playlist.kt
+++ src/main/kotlin/Playlist.kt
@@ -1,2 +1,36 @@
-class Playlist {
+import interfaces.ITrack
+import objects.Track
+
+class Playlist: ITrack {
+    override fun addTrack(track: Track) {
+        TODO("Not yet implemented")
+    }
+
+    override fun addTracks(tracks: List<Track>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteTrack(track: Track) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteTracks(tracks: List<Track>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getTotalDuration(): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun getNumberOfTracks(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getTrack(id:Int): Track {
+        TODO("Not yet implemented")
+    }
+
+    override fun getAllTracks(): List<Track> {
+        TODO("Not yet implemented")
+    }
 }
---
+++ src/main/kotlin/interfaces/ITrack.kt
@@ -0,0 +1,14 @@
+package interfaces
+
+import objects.Track
+
+interface ITrack {
+    fun addTrack(track: Track)
+    fun addTracks(tracks: List<Track>)
+    fun deleteTrack(track: Track)
+    fun deleteTracks(tracks: List<Track>)
+    fun getTotalDuration(): Long
+    fun getNumberOfTracks(): Int
+    fun getTrack(id:Int):Track
+    fun getAllTracks():List<Track>
+}
